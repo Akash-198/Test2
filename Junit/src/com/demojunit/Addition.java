aada7576e931aa2242eadf03206faea7cceea21c
Akash <Akash@Akash>
2018-11-13 12:57:10 +0530

    junit

---
+++ Junit/src/com/demojunit/Addition.java
@@ -0,0 +1,9 @@
+package com.demojunit;
+
+public class Addition { 
+
+public int add(int a, int b) { 
+return a + b; 
+} 
+} 
+
---
+++ Junit/src/com/demojunit/AdditionTest.java
@@ -0,0 +1,14 @@
+package com.demojunit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AdditionTest { 
+@Test 
+public void addIntTest() { 
+Addition addition = new Addition(); 
+int sum = addition.add(20, 30); 
+Assert.assertEquals(50, sum); 
+} 
+} 
+
